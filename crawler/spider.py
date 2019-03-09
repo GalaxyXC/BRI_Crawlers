da939031c54b6f1864625a6b402a199baedb2de3
Lance <aquadong0227@gmail.com>
2019-03-09 00:00:03 -0500

    edited crawler behavior, new _crawl() method, create_job() method, new fields in websites.json

--- crawler/spider.py
+++ crawler/spider.py
@@ -1,12 +1,15 @@
+import re
 import json
 import time
 import datetime
 from lxml import etree
 
-from crawler.utils import *
+import crawler.utils as utils
 
-KEYWORDS_FILE = "./static/keywords.json"
-WEBSITE_FILE = "./static/websites.json"
+FILE_DIR = "crawler/"
+FILE_DIR = "./"
+KEYWORDS_FILE = "static/keywords.json"
+WEBSITE_FILE = "static/websites.json"
 
 
 class BeltRoadSpider:
@@ -17,25 +20,23 @@ class BeltRoadSpider:
         self.start_date = start_date
         self.urls_to_return = []
 
-    def crawl(self, domain, link, mode, mode_int, args):
+    def crawl(self, mode_int, args):
         """
         call self.crawl(*) according to mode passed in
-        :param domain: domain url
-        :param link: typically url of a search page or list page under news channel
-        :param mode: 'BY_PAGE or 'BY_DATE'
         :param mode_int: integer indicating which _crawl(*) to call
-        :param args:
-            if mode == 'BY_PAGE':
-                args[0]: int: max. page corresponding to the starting page,
-                args[1]: string: representing the page parameter string in url
-            else:
-                args: ?: max date
+        :param args: arguments parsed to the designated crawler
         :return: a list of urls that meet the search criteria, using bri_country_list and action_words
         """
-        if mode_int == 0:
-            return self._crawl0(domain, link, mode, args)
+        if mode_int < 0:
+            return ""
 
-    def _crawl0(self, domain, link, mode, args):
+        if mode_int == 0:
+            return self._crawl0(args)
+
+        if mode_int == 1:
+            return self._crawl1(args)
+
+    def _crawl0(self, args):
         """
         mode 0:
         search page url: domain+link+"&cur_page="
@@ -43,68 +44,191 @@ class BeltRoadSpider:
         in each news, href use relative link
         <a href="/xwzx/roll/80285.htm" target="_blank" title="XXX">XXX</a>
 
-        :param domain: domain url
-        :param link: typically url of a search page or list page under news channel
-        :param mode: 'BY_PAGE or 'BY_DATE'
-        :param args:
-            if mode == 'BY_PAGE':
-                args[0]: int: max. page corresponding to the starting page,
-                args[1]: string: representing the page parameter string in url
-            else:
-                args: ?: max date
+        :param args: name, domain, url_list, page_url_string, xpath's etc...
         :return: a list of urls that meet the search criteria, using bri_country_list and action_words
         """
+        name, domain, link = args["name"], args["domain"], args["link"]
+        page_url_prefix, page_url_suffix = args["page_url_prefix"], args["page_url_suffix"]
+        min_page, max_page = args["min_page"], args["max_page"]
+        url_xpath, title_xpath, document_xpath = args['url_xpath'], args['title_xpath'], args['document_xpath']
+        print(f"Mode 0: Crawling {name}({domain}) with xpath (title | document).")
+
         urls_to_return = []
-        max_page, page_param_string = args
-        if mode == 'BY_PAGE':
-            #page_urls = [domain+link+'&'+page_param_string+'='+str(i) for i in range(1,max_page+1)][::-1]
-            page_urls = [domain+link+'&'+page_param_string+'='+str(3)]
-            count = 0
-            for page in page_urls:
-                #document_urls = get_urls_from_page(page, 0)
+        page_urls = [domain+link+page_url_prefix+str(i)+page_url_suffix for i in range(min_page, max_page+1)][::-1]
+        #page_urls = [domain+link+page_url_prefix+str(i)+page_url_suffix+'2']
+        first_page = domain+link+page_url_prefix
+        page_urls.append(first_page)
 
-                # TEMPORARY: re-crawl with new keywords
-                with open("crawler/log.txt", 'r') as f:
-                    tmp = f.read()
-                    tmp_links = tmp.split('\n')
-                    document_urls = tmp_links[810:]
-                # END TEMPORARY
+        # Get a list of url pages (news posts) from an index page (search result)
+        for page in page_urls:
+            document_urls = utils.get_urls_from_page(page, 0, url_xpath)
+            if not document_urls:
+                print("Fail to retrieve urls from post list. ")
+            """
+            TEMPORARY: re-crawl with new keywords
+            with open("log.txt", 'r') as f:
+                tmp = f.read()
+                tmp_links = tmp.split('\n')
+                document_urls = tmp_links[810:]
+            """
 
-                for document_url in document_urls:
-                    # early stop       TODO <<< Cancel early stop when not in debug mode
-                    # count += 1
-                    # if count > 8:
-                    #     break
-                    time.sleep(3)
-                    document_html = get_html_from_url(domain+document_url)
-                    if not document_html:
-                        print(f"Fail to processed: {document_url}")
-                        with open("log.txt", "a", encoding="utf-8") as f:
-                            f.write("\n LOG: Fail to process: " + domain+document_url + "\n")
-                        continue
+            for document_url in document_urls:
+                if "url_already_has_domain" in args:
+                    document_html = utils.get_html_from_url(document_url)
+                else:
+                    document_html = utils.get_html_from_url(domain+document_url)
+                print(f"pulling: {document_url}")
 
-                    root = etree.HTML(document_html)
-                    try:
-                        title = root.xpath("//div/h1[@class='main_content_title']")[0].text
-                        document_text = root.xpath("//div[@class='content']/div/p")
-                        document_string = " ".join([e.text for e in document_text])
-                    except Exception as e:
-                        with open("log.txt", "a", encoding="utf-8") as f:
-                            f.write(f"\n LOG: Exception: {e} while parsing {domain+document_url} \n")
-                        continue
+                if not document_html:
+                    print(f"Fail to processed: {document_url}")
+                    with open("log.txt", "a", encoding="utf-8") as f:
+                        f.write("\n LOG: Fail to process: " + domain+document_url + "\n")
+                    continue
 
-                    found_log = "NOT "
-                    if self._contains_all_keywords(title+document_string):
-                        urls_to_return.append(domain+document_url)
-                        #self.urls_to_return.append(domain+document_url)
-                        found_log = ""
-                    print(f"Processed: {title}({document_url}), keyword {found_log}found. ")
-                with open("log.txt", "a", encoding="utf-8") as f:
-                    f.write("\n LOG: Crawled: "+page+"\n")
-                    f.write(f"{datetime.datetime.now()}\n")
-                    f.write("\n".join(urls_to_return))
+                root = etree.HTML(document_html)
+                try:
+                    title = root.xpath(title_xpath)[0].text
+                except Exception as et:
+                    with open("log.txt", "a", encoding="utf-8") as f:
+                        f.write(f"\n LOG: Exception: {et} while parsing Title {domain + document_url} \n")
+                    title = ""
+
+                try:
+                    document_text = root.xpath(document_xpath)
+                    document_string = " ".join([e.text for e in document_text if e.text])
+                except Exception as ed:
+                    with open("log.txt", "a", encoding="utf-8") as f:
+                        f.write(f"\n LOG: Exception: {ed} while parsing Document {domain+document_url} \n")
+                    continue
+
+                found_log = "NOT "
+                if self._contains_all_keywords(title+document_string):
+                    urls_to_return.append(domain+document_url)
+                    #urls_to_return.append(document_url)
+                    #self.urls_to_return.append(domain+document_url)
+                    found_log = ""
+
+                print(f"Processed: {title}({document_url}), keyword {found_log}found. ")
+                time.sleep(3)
+
+            with open("log.txt", "a", encoding="utf-8") as f:
+                f.write("\n LOG: Crawled: "+page+"\n")
+                f.write(f"{datetime.datetime.now()}\n")
+                f.write("\n".join(urls_to_return))
 
         return urls_to_return
+
+    def _crawl1(self, args):
+        """
+        mode 1:
+        search page url: domain+link+ (search keywords)"&cur_page="
+        http://www.ceec.net.cn/jsearch/search.do?appid=1&ck=x&pagemode=result&pos=title%2Ccontent&q=%E7%85%A4%E7%94%B5&style=1&webid=40&&p=27
+        in each news, href use relative link or full link
+        <a href="/xwzx/roll/80285.htm" target="_blank" title="XXX">XXX</a>
+
+        :param args: name, domain, url_list, page_url_string, xpath's etc...
+        :return: a list of urls that meet the search criteria, using bri_country_list and action_words
+        """
+
+        name, domain, links = args["name"], args["domain"], args["link"]
+        page_url_prefix, page_url_suffix = args["page_url_prefix"], args["page_url_suffix"]
+
+        title_xpath, document_xpath = args['title_xpath'], args['document_xpath']
+        print(f"Mode 1: Crawling {name}({domain}) with xpath (title | document).")
+
+        # Create url queues
+        if not args["read_from_file"]:
+            document_urls = self.create_jobs(args)
+        else:
+            with open(args["read_from_file"], 'r') as f:
+                document_urls = f.read().split('\n')
+
+        # Consume url queues
+        urls_to_return = []
+        count = 0
+        for document_url in document_urls:
+            count += 1
+            if args["href_pattern"] == "contain_domain":
+                url = document_url
+            elif args["href_pattern"] == "attached":
+                url = domain + document_url
+            elif args["href_pattern"] == "relative":
+                pass
+
+            print(f"[{count}] pulling : {url}")
+            document_html = utils.get_html_from_url(url)
+
+            if not document_html:
+                print(f"Fail to processed: {document_url}")
+                with open("log.txt", "a", encoding="utf-8") as f:
+                    f.write("\n LOG: Fail to process: " + domain + document_url + "\n")
+                continue
+
+            root = etree.HTML(document_html)
+            try:
+                title = root.xpath(title_xpath)[0].text
+            except Exception as et:
+                with open("log.txt", "a", encoding="utf-8") as f:
+                    f.write(f"\n LOG: Exception: {et} while parsing Title {domain + document_url} \n")
+                title = ""
+
+            try:
+                document_text = root.xpath(document_xpath)
+                document_string = " ".join([e.text for e in document_text if e.text])
+            except Exception as ed:
+                with open("log.txt", "a", encoding="utf-8") as f:
+                    f.write(f"\n LOG: Exception: {ed} while parsing Document {domain + document_url} \n")
+                continue
+
+            found_log = "NOT "
+            if self._contains_all_keywords(title + document_string):
+                urls_to_return.append(domain + document_url)
+                # urls_to_return.append(document_url)
+                # self.urls_to_return.append(domain+document_url)
+                found_log = ""
+            print(f"Processed: {document_string[:12]}(length: {len(document_string)}).")
+            print(f".. in {title}({document_url}), keyword {found_log}found. ")
+            time.sleep(3)
+
+            if count % 25 == 0:
+                with open("log.txt", "a", encoding="utf-8") as f:
+                    f.write(f"LOG: Crawled: {count} pages(Current: {document_url})\n {datetime.datetime.now()}\n")
+                    f.write("\n".join(urls_to_return))
+        return urls_to_return
+
+    def create_jobs(self, args):
+        name, domain, links = args["name"], args["domain"], args["link"]
+        page_url_prefix, page_url_suffix = args["page_url_prefix"], args["page_url_suffix"]
+
+        url_xpath = args['url_xpath']
+
+        document_urls = set()
+        for link in links:
+            # generate a list of index pages
+            min_page, max_page = link["min_page"], link["max_page"]
+            page_urls = [domain + link["url"] + page_url_prefix + str(i) + page_url_suffix \
+                         for i in range(min_page, max_page + 1)][::-1]
+            if min_page == 1:
+                first_page = domain + link["url"] + page_url_prefix
+                page_urls.append(first_page)
+
+            # Get a list of url pages (news posts) from an index page (search result)
+            for page in page_urls:
+                urls = utils.get_urls_from_page(page, 0, url_xpath)
+                time.sleep(1.3)
+                if not urls:
+                    print(f"Fail to retrieve urls from post list({page}). ")
+                document_urls.update(urls)
+        document_urls = list(document_urls)
+        file_name = name.split(' ')[0] + '_job_urls.txt'
+        with open("log.txt", "a", encoding="utf-8") as f:
+            f.write(f"\n LOG: tasks ({len(document_urls)} urls) stored in {file_name}.\n")
+        print(f"{len(document_urls)} task urls stored in {file_name}.\n")
+        with open("data/" + file_name, 'w') as f:
+            f.write("\n".join(document_urls))
+
+
+
 
     def _contains_all_keywords(self, text):
         """
@@ -125,26 +249,25 @@ class BeltRoadSpider:
 
 if __name__ == '__main__':
     CHECKLIST = ["coal_keys", "countries", "action"]
-    spider = BeltRoadSpider(KEYWORDS_FILE, CHECKLIST , None)
+    # CHECKLIST = ["coal_keys", "action"]  # test command
+    spider = BeltRoadSpider(FILE_DIR + KEYWORDS_FILE, CHECKLIST, None)
+    log = []
 
-    with open(WEBSITE_FILE, encoding="utf-8") as f:
+    with open(FILE_DIR + WEBSITE_FILE, encoding="utf-8") as f:
         websites = json.load(f)
+
     for website in websites["crawling"]:
-        data = website["data"]
-        name, domain, link = data["name"], data["domain"], data["link"]
-        crawl_mode_int, crawl_mode = data["crawl_mode_int"], data["mode"]
-        print(f"Crawling: {name}({domain}), {crawl_mode}, and mode-{crawl_mode_int}")
+        if website["crawl"] == 0:
+            continue
 
-        if crawl_mode == "BY_PAGE":
-            max_page = 1 if not data["max_page"] else data["max_page"]
-            log = spider.crawl(domain=domain, link=link,
-                               mode=crawl_mode, mode_int=crawl_mode_int, args=[max_page, data["url_page_string"]])
-        elif crawl_mode == "BY_DATE":
-            max_date = data["max_date"]
-            log = spider.crawl(domain=domain, link=link,
-                               mode=crawl_mode, mode_int=crawl_mode_int, args=max_date)
+        params = website["data"]
+        params["read_from_file"] = "crawler/data/中国南方电网_job_urls.txt"
+        # Override when read urls from file
+        params['href_pattern'] = "contain_domain"
+        log = spider.crawl(mode_int=params["crawl_mode_int"], args=params)
 
-    with open("output.txt", "w", encoding="utf-8") as f:
-        f.write("\n".join(log))
+        with open("data/" + params["name"].split(" ")[0] + "output.txt", "w", encoding="utf-8") as f:
+            f.write("\n".join(log))
 
     print(f"LOG: Crawled {len(log)} links that contains all keywords in {CHECKLIST}. ")
+
--- crawler/utils.py
+++ crawler/utils.py
@@ -1,3 +1,4 @@
+import re
 import requests
 from lxml import etree
 
@@ -6,7 +7,7 @@ USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " \
              "Chrome/72.0.3626.109 Safari/537.36"
 
 
-def get_urls_from_page(page_url, mode):
+def get_urls_from_page(page_url, mode, args):
     """
     Calls get_urls_from_page(*)
     :param page_url: url of page
@@ -14,23 +15,34 @@ def get_urls_from_page(page_url, mode):
     :return: List[string], a list of urls
     """
     if mode == 0:
-        return _get_urls_from_page0(page_url)
+        return _get_urls_from_page0(page_url, url_xpath=args)
+    elif mode == 1:
+        return _get_urls_from_page0(page_url, url_xpath=args)
 
 
-def _get_urls_from_page0(page_url):
+def _get_urls_from_page0(page_url, url_xpath):
     # parsing YiDaiYiLuWang's news list
     html = get_html_from_url(page_url)
     if not html:
-        print("Fail to retrieve urls from post list. ")
         return []
 
     root = etree.HTML(html)
-    a_elements = root.xpath("//ul[@class='commonList_dot']/li/a")
+    url_elements = root.xpath(url_xpath)
+    return [elem.get("href") for elem in url_elements]
 
-    urls_to_return = []
-    for a_element in a_elements:
-        urls_to_return.append(a_element.get('href'))
-    return urls_to_return
+
+
+def _get_urls_from_page0(page_url, url_xpath):
+    # parsing YiDaiYiLuWang's news list
+    html = get_html_from_url(page_url)
+    if not html:
+        return []
+
+    root = etree.HTML(html)
+    url_elements = root.xpath(url_xpath)
+    return [elem.get("href") for elem in url_elements]
+
+
 
 
 def get_post_date_from_document(document, mode):
@@ -49,3 +61,48 @@ def get_html_from_url(url):
         return r.text
     else:
         return ""
+
+def remove_domain_prefix(path, file, domain):
+    with open(path+file, 'r') as f:
+        text = f.read()
+    lines = text.split('\n')
+    ret = []
+    for line in lines:
+        ret.append(line.replace(domain, '', 1))
+    new_text = "\n".join(ret)
+    with open(path+"new_"+file, 'w') as f:
+        f.write(new_text)
+
+def merge_urls(file1, file2):
+    with open(file1, 'r') as f:
+        text = f.read()
+    lines1 = text.split('\n')
+    with open(file2, 'r') as f:
+        text = f.read()
+    lines2 = text.split('\n')
+    unique = set(lines1)
+    unique.update(lines2)
+    with open("unique_urls.txt", 'w') as f:
+        f.write("\n".join(unique))
+
+def add_domain_by_year(file, year_prefix, year_suffix):
+    with open(file, 'r') as f:
+        urls = f.read().split('\n')
+
+    ret = []
+    for u in urls:
+        year = re.search('201[0-9]', u).group(0)
+        url = year_prefix + year + year_suffix + u.split('/', 1)[1]
+        ret.append(url)
+
+    with open("crawler/new_urls.txt", 'w') as f:
+        f.write("\n".join(ret))
+
+
+
+
+
+
+
+def parse_year(file):
+    return
